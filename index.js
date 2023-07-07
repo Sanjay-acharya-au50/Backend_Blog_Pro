fe2d51feb57a8ed8578b3c29e4e0f8db82a06d97
Sanjay <sanjayacharya992@.com>
2023-07-07 16:40:16 +0530

    c

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { isatty } = require("tty");
 const app = express();
 
 // middlewares
-app.use(cors({ credentials: true, origin: "http://localhost:5000" }));
+app.use(cors({ credentials: true, origin: "https://frontend-blog-project-one.vercel.app" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
